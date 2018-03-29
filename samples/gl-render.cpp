48d8844420588b9df0b55a0b562b6c19d3e795bb
dimitri <ddiakopoulos@gmail.com>
2018-03-29 10:10:04 -0700

    begin sketching out empty sample app for the low-level gl api

---
+++ samples/gl-render.cpp
@@ -0,0 +1,92 @@
+#include "index.hpp"
+
+using namespace polymer;
+
+struct sample_gl_render final : public GLFWApp
+{
+    perspective_camera cam;
+    FlyCameraController flycam;
+
+    sample_gl_render();
+    ~sample_gl_render();
+
+    void on_window_resize(int2 size) override;
+    void on_input(const InputEvent & event) override;
+    void on_update(const UpdateEvent & e) override;
+    void on_draw() override;
+};
+
+sample_gl_render::sample_gl_render() : GLFWApp(1280, 720, "sample-gl-render")
+{
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    cam.look_at({ 0, 9.5f, -6.0f }, { 0, 0.1f, 0 });
+    flycam.set_camera(&cam);
+}
+
+sample_gl_render::~sample_gl_render()
+{ 
+
+}
+
+void sample_gl_render::on_window_resize(int2 size)
+{ 
+
+}
+
+void sample_gl_render::on_input(const InputEvent & event)
+{
+    flycam.handle_input(event);
+}
+
+void sample_gl_render::on_update(const UpdateEvent & e)
+{
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);;
+}
+
+void sample_gl_render::on_draw()
+{
+    glfwMakeContextCurrent(window);
+
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    glViewport(0, 0, width, height);
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    const Pose cameraPose = cam.pose;
+    const float4x4 projectionMatrix = cam.get_projection_matrix(float(width) / float(height));
+    const float4x4 viewMatrix = cam.get_view_matrix();
+    const float4x4 viewProjectionMatrix = mul(projectionMatrix, viewMatrix);
+
+    gl_check_error(__FILE__, __LINE__);
+
+    glfwSwapBuffers(window); 
+}
+
+IMPLEMENT_MAIN(int argc, char * argv[])
+{
+    try
+    {
+        sample_gl_render app;
+        app.main_loop();
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << "Application Fatal: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
