bd84620b7803c4d46ee4c3e79d20a0bdd102d03b
dimitri <ddiakopoulos@gmail.com>
2018-04-28 19:24:07 -0700

    begin ecs lib-engine refactoring

--- lib-engine/ecs/system-name.hpp
+++ lib-engine/ecs/system-name.hpp
@@ -1,90 +1,101 @@
 #pragma once
 
+#ifndef polymer_system_name_hpp
+#define polymer_system_name_hpp
+
 #include "typeid.hpp"
-#include "base-ecs.hpp"
+#include "core-ecs.hpp"
 
 ////////////////////////
 //   name/id system   // 
 ////////////////////////
 
-class name_system final : public base_system
+namespace polymer
 {
 
-    std::unordered_map<entity, std::string> entity_to_name_;
-    std::unordered_map<entity, poly_hash_value> entity_to_hash_;
-    std::unordered_map<poly_hash_value, entity> hash_to_entity_;
-
-public:
-
-    name_system(entity_orchestrator * orch) : base_system(orch)
+    class name_system final : public base_system
     {
-        register_system_for_type(this, hash_fnv1a("std::string"));
-    }
 
-    ~name_system() override { }
+        std::unordered_map<entity, std::string> entity_to_name_;
+        std::unordered_map<entity, poly_hash_value> entity_to_hash_;
+        std::unordered_map<poly_hash_value, entity> hash_to_entity_;
 
-    virtual bool create(entity e, poly_typeid hash, void * data) override final
-    {
-        if (hash_fnv1a("std::string") != hash) { return false; }
-        if (!get_name(e).empty()) throw std::runtime_error("duplicate names are not permitted");
-        const std::string name = *static_cast<std::string *>(data);
-        return set_name(e, name);
-    }
+    public:
 
-    bool create(entity e, const std::string & name)
-    {
-        if (!get_name(e).empty()) throw std::runtime_error("duplicate names are not permitted");
-        return set_name(e, name);
-    }
-
-    // Disassociates any name from the entity
-    void destroy(entity entity) override final
-    {
-        auto iter = entity_to_hash_.find(entity);
-        if (iter != entity_to_hash_.end())
+        name_system(entity_orchestrator * orch) : base_system(orch)
         {
-            hash_to_entity_.erase(iter->second);
-            entity_to_hash_.erase(iter);
+            register_system_for_type(this, hash_fnv1a("std::string"));
         }
-        entity_to_name_.erase(entity);
-    }
 
-    // Finds the name associated with the entity. Returns empty string if no name is found.
-    std::string get_name(entity entity) const
-    {
-        const auto iter = entity_to_name_.find(entity);
-        return iter != entity_to_name_.end() ? iter->second : "";
-    }
+        ~name_system() override { }
 
-    bool set_name(entity entity, const std::string & name)
-    {
-        if (entity == kInvalidEntity) { return false; }
+        virtual bool create(entity e, poly_typeid hash, void * data) override final
+        {
+            if (hash_fnv1a("std::string") != hash) { return false; }
+            if (!get_name(e).empty()) throw std::runtime_error("duplicate names are not permitted");
+            const std::string name = *static_cast<std::string *>(data);
+            return set_name(e, name);
+        }
 
-        const std::string existing_name = get_name(entity);
-        if (existing_name == name) { return false; } // No need to proceed if current name and desired name are identical
+        bool create(entity e, const std::string & name)
+        {
+            if (!get_name(e).empty()) throw std::runtime_error("duplicate names are not permitted");
+            return set_name(e, name);
+        }
 
-        // Ensure a different entity with the same name does not already exist. This
-        // may happen if an entity with the name had not been properly deleted or
-        // the same entity had been created multiple times.
-        if (find_entity(name) != kInvalidEntity) return false; // fail silently
-  
-        const auto hash = hash_fnv1a(name.c_str());
+        // Disassociates any name from the entity
+        void destroy(entity entity) override final
+        {
+            auto iter = entity_to_hash_.find(entity);
+            if (iter != entity_to_hash_.end())
+            {
+                hash_to_entity_.erase(iter->second);
+                entity_to_hash_.erase(iter);
+            }
+            entity_to_name_.erase(entity);
+        }
 
-        hash_to_entity_.erase(hash_fnv1a(existing_name.c_str()));
-        hash_to_entity_[hash] = entity;
+        // Finds the name associated with the entity. Returns empty string if no name is found.
+        std::string get_name(entity entity) const
+        {
+            const auto iter = entity_to_name_.find(entity);
+            return iter != entity_to_name_.end() ? iter->second : "";
+        }
 
-        entity_to_name_[entity] = name;
-        entity_to_hash_[entity] = hash;
+        bool set_name(entity entity, const std::string & name)
+        {
+            if (entity == kInvalidEntity) { return false; }
 
-        return true;
-    }
+            const std::string existing_name = get_name(entity);
+            if (existing_name == name) { return false; } // No need to proceed if current name and desired name are identical
 
-    entity find_entity(const std::string & name) const
-    {
-        const auto hash = hash_fnv1a(name.c_str());
-        const auto iter = hash_to_entity_.find(hash);
-        return iter != hash_to_entity_.end() ? iter->second : kInvalidEntity;
-    }
-};
+            // Ensure a different entity with the same name does not already exist. This
+            // may happen if an entity with the name had not been properly deleted or
+            // the same entity had been created multiple times.
+            if (find_entity(name) != kInvalidEntity) return false; // fail silently
 
-POLYMER_SETUP_TYPEID(name_system);
+            const auto hash = hash_fnv1a(name.c_str());
+
+            hash_to_entity_.erase(hash_fnv1a(existing_name.c_str()));
+            hash_to_entity_[hash] = entity;
+
+            entity_to_name_[entity] = name;
+            entity_to_hash_[entity] = hash;
+
+            return true;
+        }
+
+        entity find_entity(const std::string & name) const
+        {
+            const auto hash = hash_fnv1a(name.c_str());
+            const auto iter = hash_to_entity_.find(hash);
+            return iter != hash_to_entity_.end() ? iter->second : kInvalidEntity;
+        }
+    };
+
+    POLYMER_SETUP_TYPEID(name_system);
+
+} // end namespace polymer
+
+#endif // end polymer_system_name_hpp
+
---
+++ lib-engine/ecs/system-transform.hpp
@@ -0,0 +1,184 @@
+#pragma once
+
+#ifndef polymer_system_transform_hpp
+#define polymer_system_transform_hpp
+
+#include "typeid.hpp"
+#include "core-ecs.hpp"
+#include "component-pool.hpp"
+
+namespace polymer
+{
+
+    //////////////////////////////
+    //   Transform Components   //
+    //////////////////////////////
+
+    struct scene_graph_component : public base_component
+    {
+        scene_graph_component() {};
+        scene_graph_component(entity e) : base_component(e) {}
+        polymer::Pose local_pose;
+        polymer::float3 local_scale;
+        entity parent{ kInvalidEntity };
+        std::vector<entity> children;
+    };
+    POLYMER_SETUP_TYPEID(scene_graph_component);
+
+    struct world_transform_component : public base_component
+    {
+        world_transform_component() {};
+        world_transform_component(entity e) : base_component(e) {}
+        polymer::Pose world_pose;
+    };
+    POLYMER_SETUP_TYPEID(world_transform_component);
+
+    //////////////////////////
+    //   Transform System   //
+    //////////////////////////
+
+    class transform_system final : public base_system
+    {
+        void recalculate_world_transform(entity child)
+        {
+            auto node = scene_graph_transforms.get(child);
+            auto world_xform = world_transforms.get(child);
+
+            // If the node has a parent then we can compute a new world transform.
+            if (node->parent != kInvalidEntity)
+            {
+                auto parent_node = scene_graph_transforms.get(node->parent);
+                world_xform->world_pose = node->local_pose * parent_node->local_pose;
+            }
+            else
+            {
+                // If the node has no parent, it should be considered already in world space.
+                world_xform->world_pose = node->local_pose;
+            }
+
+            // For each child, calculate its new world transform
+            for (auto c : node->children) recalculate_world_transform(c);
+        }
+
+        void destroy_recursive(entity child)
+        {
+            auto node = scene_graph_transforms.get(child);
+            for (auto & n : node->children) destroy_recursive(n);
+
+            // Erase world transform
+            world_transforms.destroy(child);
+
+            // Erase itself after all children are gone
+            scene_graph_transforms.destroy(child);
+        }
+
+    public:
+
+        polymer_component_pool<scene_graph_component> scene_graph_transforms{ 64 };
+        polymer_component_pool<world_transform_component> world_transforms{ 64 };
+
+        transform_system(entity_orchestrator * f) : base_system(f)
+        {
+            register_system_for_type(this, get_typeid<scene_graph_component>());
+            register_system_for_type(this, get_typeid<world_transform_component>());
+        }
+
+        ~transform_system() override { }
+
+        bool create(entity e, poly_typeid hash, void * data) override final { return true; }
+
+        bool create(entity e, const polymer::Pose local_pose, const float3 local_scale)
+        {
+            const auto check_node = scene_graph_transforms.get(e);
+            const auto check_world = world_transforms.get(e);
+            if (check_node == nullptr || check_world == nullptr)
+            {
+                auto node = scene_graph_transforms.emplace(scene_graph_component(e));
+                auto world = world_transforms.emplace(world_transform_component(e));
+                node->local_pose = local_pose;
+                node->local_scale = local_scale;
+                recalculate_world_transform(e);
+                return true;
+            }
+            return false;
+        }
+
+        bool has_transform(entity e) const
+        {
+            return (scene_graph_transforms.get(e) != nullptr);
+        }
+
+        bool add_child(entity parent, entity child)
+        {
+            if (parent == kInvalidEntity) throw std::invalid_argument("parent was invalid");
+            if (child == kInvalidEntity) throw std::invalid_argument("child was invalid");
+            if (!has_transform(parent)) throw std::invalid_argument("parent has no transform component");
+            if (!has_transform(child)) throw std::invalid_argument("child has no transform component");
+
+            scene_graph_transforms.get(parent)->children.push_back(child);
+            scene_graph_transforms.get(child)->parent = parent;
+            recalculate_world_transform(parent);
+            return true;
+        }
+
+        void insert_child(entity parent, entity child, uint32_t idx = -1) { /* todo */ }
+
+        void move_child(entity child, uint32_t idx) { /* todo */ }
+
+        const scene_graph_component * get_local_transform(entity e)
+        {
+            if (e == kInvalidEntity) return nullptr;
+            return scene_graph_transforms.get(e);
+        }
+
+        const world_transform_component * get_world_transform(entity e)
+        {
+            if (e == kInvalidEntity) return nullptr;
+            return world_transforms.get(e);
+        }
+
+        bool update_local_transform(entity e, const Pose new_pose)
+        {
+            if (e == kInvalidEntity) return kInvalidEntity;
+            if (auto * node = scene_graph_transforms.get(e))
+            {
+                node->local_pose = new_pose;
+                recalculate_world_transform(e);
+                return true;
+            }
+            return false;
+        }
+
+        entity get_parent(entity child) const
+        {
+            if (child == kInvalidEntity) return kInvalidEntity;
+            if (auto * e = scene_graph_transforms.get(child)) return e->parent;
+            return kInvalidEntity;
+        }
+
+        void remove_parent(entity child)
+        {
+            if (child == kInvalidEntity) throw std::invalid_argument("entity was invalid");
+            auto child_node = scene_graph_transforms.get(child);
+            if (child_node->parent != kInvalidEntity)
+            {
+                auto parent_node = scene_graph_transforms.get(child_node->parent);
+                parent_node->children.erase(std::remove(parent_node->children.begin(), parent_node->children.end(), child), parent_node->children.end());
+                child_node->parent = kInvalidEntity;
+                recalculate_world_transform(child);
+            }
+        }
+
+        void destroy(entity e) override final
+        {
+            if (e == kInvalidEntity) throw std::invalid_argument("entity was invalid");
+            if (!has_transform(e)) throw std::invalid_argument("no component exists for this entity");
+            destroy_recursive(e);
+        }
+    };
+
+    POLYMER_SETUP_TYPEID(transform_system);
+
+} // end namespace polymer
+
+#endif // end polymer_system_transform_hpp
