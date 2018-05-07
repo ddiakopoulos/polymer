0e44e8aa6598f8a1f8055304828dca0810942843
dimitri <ddiakopoulos@gmail.com>
2018-05-07 09:19:29 -0700

    stub file for memory pool

---
+++ lib-polymer/memory-pool.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef polymer_memory_pool_hpp
+#define polymer_memory_pool_hpp
+
+#include <stdint.h>
+#include <vector>
+#include <type_traits>
+#include <limits>
+
+namespace polymer
+{
+
+};
+
+#endif
