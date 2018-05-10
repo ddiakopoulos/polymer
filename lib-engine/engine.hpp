dfb589bfe0fab8194e5a0f5eb864af1ddaae0cbe
dimitri <ddiakopoulos@gmail.com>
2018-05-10 05:39:35 -0700

    additional gfx headers added to engine.hpp

--- lib-engine/engine.hpp
+++ lib-engine/engine.hpp
@@ -15,7 +15,6 @@
 
 #include "renderer-standard.hpp"
 #include "renderer-debug.hpp"
-
 #include "openvr-hmd.hpp"
 #include "openvr-camera.hpp"
 
@@ -25,5 +24,21 @@
 #include "ecs/system-name.hpp"
 #include "ecs/system-transform.hpp"
 #include "ecs/typeid.hpp"
+
+#include "gl-api.hpp"
+#include "gl-async-gpu-timer.hpp"
+#include "gl-camera.hpp"
+#include "gl-gizmo.hpp"
+#include "gl-imgui.hpp"
+#include "gl-loaders.hpp"
+#include "gl-mesh.hpp"
+#include "gl-nvg.hpp"
+#include "gl-procedural-mesh.hpp"
+#include "gl-procedural-sky.hpp"
+#include "gl-renderable-grid.hpp"
+#include "gl-renderable-meshline.hpp"
+#include "gl-texture-view.hpp"
+
+#include "glfw-app.hpp"
 
 #endif // end polymer_engine_hpp
