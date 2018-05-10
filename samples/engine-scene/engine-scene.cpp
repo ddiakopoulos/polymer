1ee46ab849bc19e75789738c870a9dcdd9f5414d
dimitri <ddiakopoulos@gmail.com>
2018-05-10 05:28:14 -0700

    empty sample app for further dev

---
+++ samples/engine-scene/engine-scene.cpp
@@ -0,0 +1,87 @@
+/*
+ * File: samples/engine-scene.cpp
+ */
+
+#include "index.hpp"
+#include "gl-loaders.hpp"
+
+using namespace polymer;
+
+struct sample_engine_scene final : public polymer_app
+{
+    perspective_camera cam;
+
+    sample_engine_scene();
+    ~sample_engine_scene();
+
+    void on_window_resize(int2 size) override;
+    void on_input(const app_input_event & event) override;
+    void on_update(const app_update_event & e) override;
+    void on_draw() override;
+};
+
+sample_engine_scene::sample_engine_scene() : polymer_app(1280, 720, "sample-engine-scene")
+{
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    cam.look_at({ 0, 0, 2 }, { 0, 0.1f, 0 });
+}
+
+sample_engine_scene::~sample_engine_scene() {}
+
+void sample_engine_scene::on_window_resize(int2 size) {}
+
+void sample_engine_scene::on_input(const app_input_event & event)
+{
+
+}
+
+void sample_engine_scene::on_update(const app_update_event & e)
+{
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+}
+
+void sample_engine_scene::on_draw()
+{
+    glfwMakeContextCurrent(window);
+
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    glViewport(0, 0, width, height);
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+
+    const Pose cameraPose = cam.pose;
+    const float4x4 projectionMatrix = cam.get_projection_matrix(float(width) / float(height));
+    const float4x4 viewMatrix = cam.get_view_matrix();
+    const float4x4 viewProjectionMatrix = mul(projectionMatrix, viewMatrix);
+
+    gl_check_error(__FILE__, __LINE__);
+
+    glfwSwapBuffers(window); 
+}
+
+int main(int argc, char * argv[])
+{
+    try
+    {
+        sample_engine_scene app;
+        app.main_loop();
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << "Application Fatal: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
