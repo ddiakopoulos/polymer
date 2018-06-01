1360569210be1bb059b2973219e5955a1a62b11c
dimitri <ddiakopoulos@gmail.com>
2018-06-01 09:27:46 -0700

    basic textured shader

---
+++ assets/shaders/renderer/textured_frag.glsl
@@ -0,0 +1,17 @@
+#include "renderer_common.glsl"
+
+in vec3 v_world_position;
+in vec3 v_view_space_position;
+in vec3 v_normal;
+in vec2 v_texcoord;
+in vec3 v_tangent;
+in vec3 v_bitangent;
+
+out vec4 f_color;
+
+uniform sampler2D s_texture;
+
+void main()
+{   
+    f_color = texture(s_texture, vec2(v_texcoord.x, v_texcoord.y));
+}
