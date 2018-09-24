1102d0b5ff70c6cbe48259ace01a2fa63deaf413
dimitri <ddiakopoulos@gmail.com>
2018-09-23 19:57:45 -0700

    stub file for polymer property

---
+++ lib-polymer/property.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef polymer_property_hpp
+#define polymer_property_hpp
+
+#include "any.hpp"
+
+namespace polymer
+{
+ 
+} // end namespace polymer
+
+#endif // end polymer_property_hpp
