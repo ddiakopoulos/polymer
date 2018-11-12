13ad06d21d4892382178072089877d7a29280b07
dimitri <ddiakopoulos@gmail.com>
2018-11-12 09:21:00 -0800

    hammersley sequence sampling functions

--- lib-polymer/math-sampling.hpp
+++ lib-polymer/math-sampling.hpp
@@ -11,6 +11,37 @@
 
 namespace polymer
 {
+
+    inline float vdc_radical_inverse(uint32_t i)
+    {
+        i = (i & 0x55555555) << 1 | (i & 0xAAAAAAAA) >> 1;
+        i = (i & 0x33333333) << 2 | (i & 0xCCCCCCCC) >> 2;
+        i = (i & 0x0F0F0F0F) << 4 | (i & 0xF0F0F0F0) >> 4;
+        i = (i & 0x00FF00FF) << 8 | (i & 0xFF00FF00) >> 8;
+        i = (i << 16) | (i >> 16);
+        return static_cast<float>(i) * 2.3283064365386963e-10f;
+    }
+
+    // Output coordinate is a sample in the +Z upper hemisphere
+    inline float3 sample_hammersley_uniform(const uint32_t i, const uint32_t n)
+    {
+        float2 coord(static_cast<float>(i) / (static_cast<float>(n), vdc_radical_inverse(i)));
+        const float phi = coord.y * 2.0f * POLYMER_PI;
+        const float t = 1.0f - coord.x;
+        const float s = sqrt(1.0f - t * t);
+        return float3(s * cos(phi), s * sin(phi), t);
+    }
+
+    // Output coordinate is a sample in the +Z upper hemisphere
+    inline float3 sample_hammersley_cosine(const uint32_t i, const uint32_t n)
+    {
+        float2 coord(static_cast<float>(i) / (static_cast<float>(n), vdc_radical_inverse(i)));
+        const float phi = coord.y * 2.0f * POLYMER_PI;
+        const float t = std::sqrt(1.0f - coord.x);
+        const float s = std::sqrt(1.0f - t * t);
+        return float3(s * std::cos(phi), s * std::sin(phi), t);
+    }
+
     // https://www.sebastiansylvan.com/post/importancesampling/
     // https://en.wikipedia.org/wiki/Stochastic_universal_sampling
     template<typename T>
