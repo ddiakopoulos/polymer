ad221fcbdb51967ef4d7e3c89e552e88574f8bf4
dimitri <ddiakopoulos@gmail.com>
2019-07-13 17:40:43 -0700

    begin fixing up the ecs performance test sample

--- samples/engine-ecs-stress/engine-ecs-stress.cpp
+++ samples/engine-ecs-stress/engine-ecs-stress.cpp
@@ -9,14 +9,17 @@
 #include "ecs/core-ecs.hpp"
 #include "environment.hpp"
 #include "renderer-util.hpp"
+#include "gl-imgui.hpp"
 
 using namespace polymer;
+using namespace gui;
 
 struct sample_engine_ecs final : public polymer_app
 {
     perspective_camera cam;
     fps_camera_controller flycam;
 
+    std::unique_ptr<imgui_instance> imgui;
     std::unique_ptr<gl_shader_monitor> shaderMonitor;
     std::unique_ptr<entity_orchestrator> orchestrator;
     std::unique_ptr<simple_texture_view> fullscreen_surface;
@@ -42,6 +45,8 @@ sample_engine_ecs::sample_engine_ecs() : polymer_app(1280, 720, "sample-ecs-stre
     glfwGetWindowSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
+    imgui.reset(new gui::imgui_instance(window, true));
+
     shaderMonitor.reset(new gl_shader_monitor("../../assets/"));
     fullscreen_surface.reset(new simple_texture_view());
     orchestrator.reset(new entity_orchestrator());
@@ -50,65 +55,43 @@ sample_engine_ecs::sample_engine_ecs() : polymer_app(1280, 720, "sample-ecs-stre
 
     scene.reset(*orchestrator, {width, height}, true);
 
-    create_handle_for_asset("debug-icosahedron", make_mesh_from_geometry(make_icosasphere(1))); // gpu mesh
-    create_handle_for_asset("debug-icosahedron", make_icosasphere(1)); // cpu mesh
+    auto icosa = make_icosasphere(1);
+    create_handle_for_asset("debug-icosahedron", make_mesh_from_geometry(icosa));
+    create_handle_for_asset("debug-icosahedron", std::move(icosa));
 
-    {   
-        uniform_random_gen rand;
-        scoped_timer create_timer("create 16384 entities");
+    uniform_random_gen rand;
+    std::vector<entity> new_entities;
 
-        std::vector<entity> new_entities;
+    // Configuring an entity at runtime programmatically
+    for (uint32_t entity_index = 0; entity_index < 16384; ++entity_index)
+    {
+        const transform pose = transform(float3(rand.random_float() * 100.f, rand.random_float() * 100.f, rand.random_float() * 100.f));
+        const float3 scale = float3(rand.random_float(0.1f, 0.5f));
+        const std::string name = "debug-icosahedron-" + std::to_string(entity_index);
 
-        // Configuring an entity at runtime programmatically
-        for (uint32_t entity_index = 0; entity_index < 16384; ++entity_index)
-        {
-            // Create a new entity to represent an icosahedron that we will render
-            const entity debug_icosa = scene.track_entity(orchestrator->create_entity());
+        const entity e = make_standard_scene_object(orchestrator.get(), &scene,
+            name, pose, scale, material_handle(material_library::kDefaultMaterialId), "debug-icosahedron", "debug-icosahedron");
 
-            const float rnd1 = rand.random_float() * 100.f;
-            const float rnd2 = rand.random_float() * 100.f;
-            const float rnd3 = rand.random_float() * 100.f;
-            const float rnd_scale = rand.random_float(0.1f, 0.5f);
+        new_entities.push_back(e);
+    }
 
-            // Give the icosa a name and default transform and scale
-            scene.identifier_system->create(debug_icosa, "debug-icosahedron-" + std::to_string(entity_index));
-            scene.xform_system->create(debug_icosa, transform(float3(rnd1, rnd2, rnd3)), float3(rnd_scale));
+    // Second pass to assemble render components separately, since `assemble_render_component` will
+    // grab pointers to components that were probably shuffled around as we inserted
+    // a bunch of them into the underlying component pool in transform_system.
+    for (uint32_t i = 0; i < new_entities.size(); ++i)
+    {
+        const auto e = new_entities[i];
 
-            // Create mesh component for the gpu mesh.
-            polymer::mesh_component mesh_component(debug_icosa);
-            mesh_component.mesh = gpu_mesh_handle("debug-icosahedron");
-            scene.render_system->create(debug_icosa, std::move(mesh_component));
-
-            // Create a geometry for the cpu mesh. This type of mesh is used for raycasting
-            // and collision, so not strictly required for this sample.
-            polymer::geometry_component geom_component(debug_icosa);
-            geom_component.geom = cpu_mesh_handle("debug-icosahedron");
-            scene.collision_system->create(debug_icosa, std::move(geom_component));
-
-            // Create material component with a default (normal-mapped) material
-            polymer::material_component material_component(debug_icosa);
-            material_component.material = material_handle(material_library::kDefaultMaterialId);
-            scene.render_system->create(debug_icosa, std::move(material_component));
-
-            new_entities.push_back(debug_icosa);
-        }
-
-        // Second pass to assemble render components separately, since `assemble_render_component` will
-        // grab pointers to components that were probably shuffled around as we inserted
-        // a bunch of them into the underlying component pool in transform_system.
-        for (uint32_t i = 0; i < new_entities.size(); ++i)
-        {
-            const auto e = new_entities[i];
-
-            // Assemble a render_component (gather components so the renderer does not have to interface
-            // with many systems). Ordinarily this assembly is done per-frame in the update loop, but
-            // this is a fully static scene.
-            payload.render_components.emplace_back(assemble_render_component(scene, e));
-        }
+        // Assemble a render_component (gather components so the renderer does not have to interface
+        // with many systems). Ordinarily this assembly is done per-frame in the update loop, but
+        // this is a fully static scene.
+        payload.render_components.emplace_back(assemble_render_component(scene, e));
     }
 
     cam.look_at({ 0, 0, 2 }, { 0, 0.1f, 0 });
     flycam.set_camera(&cam);
+
+    cam.farclip = 128.f;
 
     scene.resolver->add_search_path("../../assets/");
     scene.resolver->resolve();
@@ -121,6 +104,7 @@ void sample_engine_ecs::on_window_resize(int2 size) {}
 void sample_engine_ecs::on_input(const app_input_event & event)
 {
     flycam.handle_input(event);
+    imgui->update_input(event);
 }
 
 void sample_engine_ecs::on_update(const app_update_event & e)
@@ -156,11 +140,20 @@ void sample_engine_ecs::on_draw()
 
     fullscreen_surface->draw(scene.render_system->get_renderer()->get_color_texture(viewIndex));
 
-    // Optional debug output
+    imgui->begin_frame();
+
+    ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     for (auto & t : scene.render_system->get_renderer()->cpuProfiler.get_data())
     {
-        std::cout << "[render_system CPU] " << t.first.c_str() << " - " << t.second << "ms" << std::endl;
+        ImGui::Text("CPU: %s - %f", t.first.c_str(), t.second);
     }
+
+    for (auto & t : scene.render_system->get_renderer()->gpuProfiler.get_data())
+    {
+        ImGui::Text("GPU: %s - %f", t.first.c_str(), t.second);
+    }
+
+    imgui->end_frame();
 
     gl_check_error(__FILE__, __LINE__);
 
